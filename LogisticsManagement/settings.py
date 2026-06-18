a31e608d583291fee2cd80f8ed83857a09d6ed7e
Sreehari-S6 <sreeharisajeev2006@gmail.com>
2026-06-18 20:54:30 +0530

    Switch database to PostgreSQL

--- LogisticsManagement/settings.py
+++ LogisticsManagement/settings.py
@@ -86,15 +86,12 @@ WSGI_APPLICATION = 'LogisticsManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
